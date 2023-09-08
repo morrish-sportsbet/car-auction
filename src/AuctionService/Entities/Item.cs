6085a1d3199f1bfefeff7a17d5497c68365d018a
Morris Hazrati <morris.hazrati@sportsbet.com.au>
2023-09-08 17:38:11 +1000

    Added DbContext

--- src/AuctionService/Entities/Item.cs
+++ src/AuctionService/Entities/Item.cs
@@ -1,10 +1,12 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace AuctionService.Entities
 {
+    [Table("Items")]
     public class Item
     {
         public Guid Id { get; set; }
