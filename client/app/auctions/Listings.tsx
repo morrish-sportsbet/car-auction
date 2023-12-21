4bed56773568789cb87b78b0aa83ad1170e33ed1
Morris Hazrati <morris.hazrati@sportsbet.com.au>
2023-12-21 15:47:49 +1100

    setup a store with Zustand. Created CarCard and used it into Listings. Added prettierrc and vscode settings. added needed types

---
+++ client/app/auctions/Listings.tsx
@@ -0,0 +1,47 @@
+"use client"
+
+import React, { useEffect, useState } from "react"
+import AuctionCard from "./AuctionCard"
+import { Auction, PagedResult } from "@/types"
+import { getData } from "../actions/auctionActions"
+import { useParamsStore } from "../../store/useStore"
+import { shallow } from "zustand/shallow"
+import qs from "query-string"
+
+export default function Listings() {
+	const [data, setData] = useState<PagedResult<Auction>>()
+	const params = useParamsStore(
+		state => ({
+			pageNumber: state.pageNumber,
+			pageSize: state.pageSize,
+			searchTerm: state.searchTerm,
+			orderBy: state.orderBy,
+			filterBy: state.filterBy
+		}),
+		shallow
+	)
+	const setParams = useParamsStore(state => state.setParams)
+	const url = qs.stringifyUrl({ url: "", query: params })
+
+	function setPageNumber(pageNumber: number) {
+		setParams({ pageNumber })
+	}
+
+	useEffect(() => {
+		getData(url).then(data => {
+			setData(data)
+		})
+	}, [url])
+
+	if (!data) return <h3>Loading...</h3>
+
+	return (
+		<>
+			<div className="grid grid-cols-4 gap-6">
+				{data.results.map(auction => (
+					<AuctionCard auction={auction} key={auction.id} />
+				))}
+			</div>
+		</>
+	)
+}
