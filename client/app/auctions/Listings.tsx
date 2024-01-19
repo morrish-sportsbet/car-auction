20e9011510c2f07e38b7bf39cca16e822f2a5ae1
Morris Hazrati <morris.hazrati@sportsbet.com.au>
2024-01-19 16:59:49 +1100

    added filters and pagination, not working yet, just the UI

--- client/app/auctions/Listings.tsx
+++ client/app/auctions/Listings.tsx
@@ -3,10 +3,13 @@
 import React, { useEffect, useState } from "react"
 import AuctionCard from "./AuctionCard"
 import { Auction, PagedResult } from "@/types"
+import AppPagination from "../components/AppPagination"
 import { getData } from "../actions/auctionActions"
-import { useParamsStore } from "../../store/useStore"
+import Filters from "./Filters"
+import { useParamsStore } from "@/hooks/useParamsStore"
 import { shallow } from "zustand/shallow"
 import qs from "query-string"
+import EmptyFilter from "../components/EmptyFilter"
 
 export default function Listings() {
 	const [data, setData] = useState<PagedResult<Auction>>()
@@ -37,11 +40,25 @@ export default function Listings() {
 
 	return (
 		<>
-			<div className="grid grid-cols-4 gap-6">
-				{data.results.map(auction => (
-					<AuctionCard auction={auction} key={auction.id} />
-				))}
-			</div>
+			<Filters />
+			{data.totalCount === 0 ? (
+				<EmptyFilter showReset />
+			) : (
+				<>
+					<div className="grid grid-cols-4 gap-6">
+						{data.results.map(auction => (
+							<AuctionCard auction={auction} key={auction.id} />
+						))}
+					</div>
+					<div className="flex justify-center mt-4">
+						<AppPagination
+							pageChanged={setPageNumber}
+							currentPage={params.pageNumber}
+							pageCount={data.pageCount}
+						/>
+					</div>
+				</>
+			)}
 		</>
 	)
 }
